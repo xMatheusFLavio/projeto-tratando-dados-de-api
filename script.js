93ac90524a4264b4a69fa602a605999fbf13c50d
Matheus <matheusflavio888@gmail.com>
2023-06-14 08:25:16 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,38 @@
+async function buscaEndereco(cep) {
+var mensagemErro = document.getElementById('erro');
+mensagemErro.innerHTML = "";
+try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+        throw Error('CEP não existente!');
+    }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+
+    cidade.value = consultaCEPConvertida.localidade
+    logradouro.value = consultaCEPConvertida.logradouro
+    estado.value = consultaCEPConvertida.uf
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP invalido. Tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+
+
+
+
+
+
+// let ceps = ['01001000', '01001001']
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// console.log(conjuntoCeps);
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
